9c32a40e74e51449c2735e595473e2dc28bd91b6
Carol D. Toro <toroc@oregonstate.edu>
2016-11-10 16:36:12 -0800

    Added module 1 completed assignment, exceed calls to google map provider

---
+++ oop-java/week-2/src/ArrayLocation.java
@@ -0,0 +1,21 @@
+package src;
+
+public class ArrayLocation {
+	
+	private double coords[];
+	
+	//	Constructor	
+	public ArrayLocation(double[] coords)
+	{
+		this.coords = coords;
+	}
+	
+	public static void main(String[] args)
+	{
+		double[] coords = {5.0, 0.0};
+		ArrayLocation accra = new ArrayLocation(coords);
+		coords[0] = 32.9;
+		coords[1] = -117.2;
+		System.out.println(accra.coords[0]);
+	}
+}
